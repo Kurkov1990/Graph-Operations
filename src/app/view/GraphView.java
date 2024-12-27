1d91d0f1941294a4e6b1b341bc953c79b3a81cc7
Andrii Kurkov <kurkovandrey90@gmail.com>
2024-12-27 10:38:38 +0200

    Update user's menu

--- src/app/view/GraphView.java
+++ src/app/view/GraphView.java
@@ -7,16 +7,18 @@ public class GraphView {
     Scanner scanner = new Scanner(System.in);
 
     public int getMenuChoice() {
-        System.out.println("\nGraph Operations:");
-        System.out.println("1. Add Vertex");
-        System.out.println("2. Add Edge");
-        System.out.println("3. Remove Vertex");
-        System.out.println("4. Remove Edge");
-        System.out.println("5. Verify Vertex");
-        System.out.println("6. Verify Edge");
-        System.out.println("7. Show Graph");
-        System.out.println("8. Exit");
-        System.out.print("\nChoose an option to Continue: ");
+        String menu = """
+                Graph Operations:
+                1. Add Vertex
+                2. Add Edge
+                3. Remove Vertex
+                4. Remove Edge
+                5. Verify Vertex
+                6. Verify Edge
+                7. Show Graph
+                8. Exit
+                """;
+        System.out.println(menu);
         return scanner.nextInt();
     }
 
